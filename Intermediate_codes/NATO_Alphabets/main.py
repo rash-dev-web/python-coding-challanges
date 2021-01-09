5c0c1bd8ec505f8fbbfdd3c2a713f0f1bbcd7f4a
Rasheed Mohammad <rasheed.m@live.com>
2021-01-09 16:57:20 +0530

    added exception handling in password manager & nato alphabets

--- Intermediate_codes/NATO_Alphabets/main.py
+++ Intermediate_codes/NATO_Alphabets/main.py
@@ -24,6 +24,7 @@
 # TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}
 import pandas
+
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 # data_dict = {
 #     "letter": data.letter.to_list(),
@@ -36,7 +37,30 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(alphabet_dict)
 
+
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter your name: ").upper()
-user_name_list = [alphabet_dict[letter] for letter in user_input]
-print(user_name_list)
+# one way of implementation without function
+# do_continue = True
+# while do_continue:
+#     user_input = input("Enter your name: ").upper()
+#     try:
+#         user_name_list = [alphabet_dict[letter] for letter in user_input]
+#     except KeyError:
+#         print("Sorry, only letters in the alphabet please")
+#     else:
+#         print(user_name_list)
+#         do_continue = False
+
+# with function
+def generate_phonetic():
+    user_input = input("Enter your name: ").upper()
+    try:
+        user_name_list = [alphabet_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(user_name_list)
+
+
+generate_phonetic()
--- Intermediate_codes/password_manager/main.py
+++ Intermediate_codes/password_manager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -32,21 +33,62 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    with open("data.txt", "a") as file:
-        website = website_textbox.get()
-        email_username = email_username_textbox.get()
-        password = password_textbox.get()
+    # changed the file format to json in write mode
+    website = website_textbox.get()
+    email_username = email_username_textbox.get()
+    password = password_textbox.get()
+    new_data = {
+        website: {
+            "email": email_username,
+            "password": password,
+        }
+    }
 
-        if len(website) == 0 or len(email_username) == 0 or len(password) == 0:
-            messagebox.showwarning(title="Empty field", message="Don't leave any field blank")
+    if len(website) == 0 or len(email_username) == 0 or len(password) == 0:
+        messagebox.showwarning(title="Empty field", message="Don't leave any field blank")
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
         else:
-            is_ok = messagebox.askokcancel(title="Confirm", message=f"These are the details:\nWebsite:"
-                                                                    f" {website}\nEmail/Username: {email_username}\n"
-                                                                    f"Password: {password}\nIs it ok to save?")
-            if is_ok:
-                file.write(f"{website} | {email_username} | {password} \n")
-                website_textbox.delete(0, END)
-                password_textbox.delete(0, END)
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                # write to the data file
+                # added the json code to read and write data into json file
+                json.dump(data, data_file, indent=4)
+                # is_ok = messagebox.askokcancel(title="Confirm", message=f"These are the details:\nWebsite:" f" {
+                # website}\nEmail/Username: {email_username}\n" f"Password: {password}\nIs it ok to save?") if is_ok:
+                # file.write(f"{website} | {email_username} | {password} \n")
+        finally:
+            website_textbox.delete(0, END)
+            password_textbox.delete(0, END)
+
+
+# ---------------------------- search password ---------------------------#
+def search_password():
+    # get the website
+    website = website_textbox.get()
+    if len(website) == 0:
+        messagebox.showwarning(title="Input field", message="Please enter a search term")
+    else:
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+            # print(data)
+        except FileNotFoundError:
+            messagebox.showwarning(title="Error", message="No data file Found")
+        else:
+            try:
+                website_data = data[website]
+            except KeyError:
+                # this block of code can be done with if else
+                messagebox.showwarning(title="Error", message=f"No details for the {website} exists.")
+            else:
+                messagebox.showinfo(title=website, message=f"Your email: {website_data['email']}\n"
+                                                           f"Your Password: {website_data['password']}")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -84,6 +126,10 @@ password_label.grid(column=0, row=3)
 # email/username entry
 password_textbox = Entry(width=21)
 password_textbox.grid(column=1, row=3)
+
+# search button
+search_button = Button(text="Search", width=15, command=search_password)
+search_button.grid(column=2, row=1)
 
 # generate password button
 generate_pwd_button = Button(text="Generate Password", command=generate_password)
